b1ec109141da011bbf62d22eec23b7ffba74f7b0
mago mahi <maheshhonnamurthy099@gmail.com>
2022-12-05 21:37:33 +0530

    [Mahesh]Add: Leap Year

---
+++ BasicCoreProblems/LeapYear.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProblems
+{
+    internal class LeapYear
+    {
+        public static void Year()
+        {
+            Console.WriteLine("Enter the Required Year");
+            int year=Convert.ToInt32(Console.ReadLine());
+            if (( year % 4 == 0 ||  year % 400 == 0) && year % 100 != 0)
+            {
+                Console.WriteLine("The Mentioned Year Is  "+   year   +"  Leap Year");
+            }
+            else
+                Console.WriteLine("Year is not a Leap Year");
+                Console.WriteLine("Yess You Found required year.\nThank YOU");
+        }
+ 
+    }
+}
--- BasicCoreProblems/Program.cs
+++ BasicCoreProblems/Program.cs
@@ -7,7 +7,7 @@ namespace BasicCoreProblems
         public static void Main(string[] args)
         {
             Console.WriteLine("*****WELL COME TO BASICCOREPROBLEMS*****" );
-            Console.WriteLine("Please Select The Below Given option::"+ "\n" + "1.FlipCoin");
+            Console.WriteLine("Please Select The Below Given option::"+ "\n" + "1.FlipCoin"+"\n"+"2.LeapYear");
             int option = Convert.ToInt32(Console.ReadLine());
             bool flag = true;
             while (flag)
@@ -15,13 +15,19 @@ namespace BasicCoreProblems
                 switch (option)
                 {
                     case 1:
+                        Console.WriteLine("*****WELL COME TO FlipCoinProblem*****");
                         FlipCoin filpcoin = new FlipCoin();
                         filpcoin.CheckHeadTail();
+                        break;
+                    case 2:
+                        Console.WriteLine("*****WELL COME TO LeapYearProblem*****");
+                        LeapYear.Year();
                         break;
                     default:
                         flag = false;
                         Console.WriteLine("Please Enter Valid option");
                         break;
+
                 }
                 
             }
