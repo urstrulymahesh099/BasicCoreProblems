a58b22b30f0738f5f0082c2f0156d1471f5ac712
mago mahi <maheshhonnamurthy099@gmail.com>
2022-12-05 20:06:38 +0530

    [Mahesh]Add: Flip Coin

---
+++ BasicCoreProblems/FlipCoin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProblems
+{
+    internal class FlipCoin
+    {
+        double percentageOfHead = 0, percentageOfTail = 0;
+        double headCount=0,tailCount=0;
+
+        public void CheckHeadTail()
+        {
+            Console.WriteLine("Enter the number of times want to Filp the Coins:");
+            int noOfCountTimes=Convert.ToInt32(Console.ReadLine());
+            for(int i=0;i<noOfCountTimes;i++)
+            {
+               Random randobj = new Random();
+               randobj.Next();
+                {
+                    if(randobj.Next(1,2) <0.5)
+                    {
+                        tailCount++;
+                        Console.WriteLine("TailCount :"+ tailCount);
+                    }
+                    else
+                        headCount++;
+                        Console.WriteLine("HeadCount :" + headCount);
+
+                    Console.WriteLine("---->>>>>PERCENTAGE OF HEAD AND TAIL COUNT<<<<<-----");
+                    percentageOfHead = headCount / noOfCountTimes * 100;
+                    percentageOfTail = tailCount / noOfCountTimes * 100;
+                    Console.WriteLine("Percentage of Head Count :" + percentageOfHead + "%");
+                    Console.WriteLine("Percentage of Tail Count :" + percentageOfTail + "%");
+                    Console.WriteLine("******Thank YOU*******");
+
+                }               
+            }
+
+        }
+    }
+}
--- BasicCoreProblems/Program.cs
+++ BasicCoreProblems/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace BasicCoreProblems
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("*****WELL COME TO BASICCOREPROBLEMS*****" );
+            Console.WriteLine("Please Select The Below Given option::"+ "\n" + "1.FlipCoin");
+            int option = Convert.ToInt32(Console.ReadLine());
+            bool flag = true;
+            while (flag)
+            {
+                switch (option)
+                {
+                    case 1:
+                        FlipCoin filpcoin = new FlipCoin();
+                        filpcoin.CheckHeadTail();
+                        break;
+                    default:
+                        flag = false;
+                        Console.WriteLine("Please Enter Valid option");
+                        break;
+                }
+                
+            }
+               
+
+
+
+            }
+        }
+    }
