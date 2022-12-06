4096e7f24bef31ffa375d5d222cf9e51d1757e7d
mago mahi <maheshhonnamurthy099@gmail.com>
2022-12-06 21:34:31 +0530

    [mahesh]Add:EvenOddNumber

--- BasicCoreProblems/EvenOddNumber.cs
+++ BasicCoreProblems/EvenOddNumber.cs
@@ -8,5 +8,15 @@ namespace BasicCoreProblems
 {
     internal class EvenOddNumber
     {
+        public static void Number()
+        {
+            Console.WriteLine("Enter Value");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if (num % 2 == 0)
+                Console.WriteLine("Number is even number");
+            else
+                Console.WriteLine("Number is odd number");
+        }
+
     }
 }
--- BasicCoreProblems/Program.cs
+++ BasicCoreProblems/Program.cs
@@ -7,7 +7,7 @@ namespace BasicCoreProblems
         public static void Main(string[] args)
         {
             Console.WriteLine("*****WELL COME TO BASICCOREPROBLEMS*****");
-            Console.WriteLine("Please Select The Below Given option::" + "\n" + "1.FlipCoin" + "\n" + "2.LeapYear" + "\n" + "3.PowerOfTwo" + "\n" + "4.HarmonicNumber"+"\n"+ "5.QuoteintAndReminder "+"\n"+ "6.SwapTwoNumbers");
+            Console.WriteLine("Please Select The Below Given option::" + "\n" + "1.FlipCoin" + "\n" + "2.LeapYear" + "\n" + "3.PowerOfTwo" + "\n" + "4.HarmonicNumber"+"\n"+ "5.QuoteintAndReminder "+"\n"+ "6.SwapTwoNumbers"+"\n"+"7.EvenAndOddNumber");
             while (true)
             {
                 int option = Convert.ToInt32(Console.ReadLine());
@@ -46,6 +46,11 @@ namespace BasicCoreProblems
                         case 6:
                             Console.WriteLine("*****WELL COME TO SwapTwoNumberProblem*****");
                             SwapTwoNumbers.Swap();
+                            Console.WriteLine("******Thank YOU*******");
+                            break;
+                        case 7:
+                            Console.WriteLine("*****WELL COME TO OddAndEvenProblem*****");
+                            EvenOddNumber.Number();
                             Console.WriteLine("******Thank YOU*******");
                             break;
                         default:
