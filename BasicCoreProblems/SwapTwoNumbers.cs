863079a3fe3a3131cccc1cca318af670e8d90c87
mago mahi <maheshhonnamurthy099@gmail.com>
2022-12-06 21:27:19 +0530

    [Mahesh]Add:Swap Numbers

--- BasicCoreProblems/SwapTwoNumbers.cs
+++ BasicCoreProblems/SwapTwoNumbers.cs
@@ -8,5 +8,19 @@ namespace BasicCoreProblems
 {
     internal class SwapTwoNumbers
     {
+        public static void Swap()
+        {
+            Console.WriteLine("Enter First Number = ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Second Number = ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            a = a + b;
+            b = a - b;
+            a = a - b;
+
+
+            Console.WriteLine("After Swap" + "\n" + "First Number Is " + a + "\n" + "Second Number Is " + b);
+        }
     }
 }
